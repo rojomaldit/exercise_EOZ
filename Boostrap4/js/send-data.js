83c0adb15c33429abbe9e9327b3671fc5dcb1f0a
rojomaldit <lucaserfurt@hotmail.com>
2020-01-15 19:38:16 -0300

    Fix overlapping TODOs List

--- Boostrap4/js/send-data.js
+++ Boostrap4/js/send-data.js
@@ -7,19 +7,31 @@ $(document).ready(function () {
         dataType: 'json',
     });
   });
-  
+
   $("#ajaxSubmit").click(function (){
     $.ajax({
       url: 'http://localhost:3000/todo/',
       type: 'GET',
       dataType: 'json',
       success: function (response) {
+
         length = response.length
+        id = response[length-1].id
         e = response[length-1].description
+
         var h = document.getElementById("TODOs");
-        h.insertAdjacentHTML("afterbegin", '<div class="col-4" id="row_todo">' + e + '</div>');
-        h.insertAdjacentHTML("afterbegin", '<div class="w-100 py-1"></div>');
-        
+        h.insertAdjacentHTML(
+          "afterbegin",
+          '<button type="button" class="btn btn-outline-primary col-md-2" id="' + id + '">Delete</button>'
+        );
+        h.insertAdjacentHTML(
+          "afterbegin", 
+          '<div class="col-4 col-md-8" id="row_todo">' + e + '</div>'
+        );
+        h.insertAdjacentHTML(
+          "afterbegin",
+          '<div class="w-100 py-1"></div>'
+        );
       }
     });
   });
