cb1835ab1fa07319bf8f0628615c05a4a52a3d04
rojomaldit <lucaserfurt@hotmail.com>
2020-01-15 12:53:46 -0300

    I can call backend

--- Boostrap4/js/send-data.js
+++ Boostrap4/js/send-data.js
@@ -4,29 +4,16 @@
 $(document).ready(function () {
   $("#ajaxSubmit").click(function (){
     alert("hola que hace")
-    $.ajax({
-      // dataType: "jsonp",
-      data: '{ "description":"John" }',
-      type: 'POST',
-      jsonpCallback: 'callback', // this is not relevant to the POST anymore
-      success: function (data) {
-        var ret = jQuery.parseJSON(data);
-        $('#lblResponse').html(ret.msg);
-        console.log('Success: ')
-      },
-      error: function (xhr, status, error) {
-        console.log('Error: ' + error.message);
-        $('#lblResponse').html('Error connecting to the server.');
-      },
-    });
-    var url = 'http://localhost:3000/todo/',
-    var data = {"description":"John"};
+    var url = 'http://localhost:3000/todo/';
+    var data = `{
+      "description":"t1_lala"
+    }`;
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
 
     //Send the proper header information along with the request
-    xhr.setRequestHeader("Content-type", data);
+    xhr.setRequestHeader("Content-type", "application/json");
 
-    xhr.send(params);
+    xhr.send(data);
   });
 })
