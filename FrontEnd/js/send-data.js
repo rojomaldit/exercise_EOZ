84b758ef9345f0dfc719dc94ca15821c99028f7d
rojomaldit <lucaserfurt@hotmail.com>
2020-01-16 10:55:45 -0300

    Change name Boostrap4 for FrontEnd and modify ajaxGet

--- FrontEnd/js/send-data.js
+++ FrontEnd/js/send-data.js
@@ -11,26 +11,28 @@ $(document).ready(function () {
       type: 'GET',
       dataType: 'json',
       success: function (response) {
-    
-        var length = response.length
-        var id = response[length-1].id
-        var e = response[length-1].description
-        var element = document.getElementById("TODOs");
-    
-        element.insertAdjacentHTML(
-          'afterbegin',
-          '<button id="btn_block'+id+'" onclick="ajaxDelete('+ id +')" class="btn btn-outline-primary col-md-2">Delete</button>'
-        );
-        element.insertAdjacentHTML(
-          "afterbegin", 
-          '<div id="des_block'+id+'" class="col-4 col-md-8">' + e + '</div>'
-        );
-        element.insertAdjacentHTML(
-          "afterbegin",
-          '<div id="spc_block'+id+'" class="w-100 py-1"></div>'
-        );
+        console.log(response)
       }
-    });
+    })
+    .then(function (response){
+      var length = response.length
+      var id = response[length-1].id
+      var e = response[length-1].description
+      var element = document.getElementById("TODOs");
+  
+      element.insertAdjacentHTML(
+        'afterbegin',
+        '<button id="btn_block'+id+'" onclick="ajaxDelete('+ id +')" class="btn btn-outline-primary col-md-2">Delete</button>'
+      );
+      element.insertAdjacentHTML(
+        "afterbegin", 
+        '<div id="des_block'+id+'" class="col-4 col-md-8">' + e + '</div>'
+      );
+      element.insertAdjacentHTML(
+        "afterbegin",
+        '<div id="spc_block'+id+'" class="w-100 py-1"></div>'
+      );
+    })
   });
 })
 
