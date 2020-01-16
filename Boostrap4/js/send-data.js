0d01f57e6be962f828b68df98940ebbdc49ed842
rojomaldit <lucaserfurt@hotmail.com>
2020-01-16 00:09:56 -0300

    Finish Delete

--- Boostrap4/js/send-data.js
+++ Boostrap4/js/send-data.js
@@ -1,38 +1,59 @@
 $(document).ready(function () {
   $("#ajaxSubmit").click(function (){
-      $.ajax({
-        url: 'http://localhost:3000/todo/',
-        type: 'POST',
-        data: {"description": $("#Add_TODO").val()},
-        dataType: 'json',
+    $.ajax({
+      url: 'http://localhost:3000/todo/',
+      type: 'POST',
+      data: {"description": $("#Add_TODO").val()},
+      dataType: 'json'
     });
-  });
-
-  $("#ajaxSubmit").click(function (){
     $.ajax({
       url: 'http://localhost:3000/todo/',
       type: 'GET',
       dataType: 'json',
       success: function (response) {
 
-        length = response.length
-        id = response[length-1].id
-        e = response[length-1].description
+        var length = response.length
+        var id = response[length-1].id
+        var e = response[length-1].description
+        var element = document.getElementById("TODOs");
 
-        var h = document.getElementById("TODOs");
-        h.insertAdjacentHTML(
-          "afterbegin",
-          '<button type="button" class="btn btn-outline-primary col-md-2" id="' + id + '">Delete</button>'
+        element.insertAdjacentHTML(
+          'afterbegin',
+          '<button id="todo_block_btn" onclick="ajaxDelete('+ id +')" class="btn btn-outline-primary col-md-2">Delete</button>'
         );
-        h.insertAdjacentHTML(
+        element.insertAdjacentHTML(
           "afterbegin", 
-          '<div class="col-4 col-md-8" id="row_todo">' + e + '</div>'
+          '<div id="todo_block_des" class="col-4 col-md-8">' + e + '</div>'
         );
-        h.insertAdjacentHTML(
+        element.insertAdjacentHTML(
           "afterbegin",
-          '<div class="w-100 py-1"></div>'
+          '<div id="todo_block_spc" class="w-100 py-1"></div>'
         );
       }
     });
   });
 })
+
+//onclick="axajDelete(' + id + ')"
+
+function ajaxDelete(id){
+    console.log("asd")
+    $.ajax({
+      url: 'http://localhost:3000/todo/' + id + '/',
+      type: 'DELETE',
+      dataType: 'json',
+      success: function (response) {
+        console.log(response)
+      }
+    });
+    var btn = document.getElementById("todo_block_btn");
+    var div_des = document.getElementById("todo_block_des");
+    var div_spc = document.getElementById("todo_block_spc");
+
+    console.log(btn, div_des, div_spc)
+    btn.remove()
+    div_des.remove()
+    div_spc.remove()
+
+    
+}
