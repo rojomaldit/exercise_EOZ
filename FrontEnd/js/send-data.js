b17fe2056c5d6a256d20306af1bc4676e4b73467
rojomaldit <lucaserfurt@hotmail.com>
2020-01-18 18:15:58 -0300

    sincronize post with get

--- FrontEnd/js/send-data.js
+++ FrontEnd/js/send-data.js
@@ -5,34 +5,36 @@ $(document).ready(function () {
       type: 'POST',
       data: {"description": $("#Add_TODO").val()},
       dataType: 'json'
-    });
-    $.ajax({
-      url: 'http://localhost:3000/todo/',
-      type: 'GET',
-      dataType: 'json',
-      success: function (response) {
-        console.log(response)
-      }
     })
-    .then(function (response){
-      var length = response.length
-      var id = response[length-1].id
-      var e = response[length-1].description
-      var element = document.getElementById("TODOs");
-  
-      element.insertAdjacentHTML(
-        'afterbegin',
-        '<button id="btn_block'+id+'" onclick="ajaxDelete('+ id +')" class="btn btn-outline-primary col-md-2">Delete</button>'
-      );
-      element.insertAdjacentHTML(
-        "afterbegin", 
-        '<div id="des_block'+id+'" class="col-4 col-md-8">' + e + '</div>'
-      );
-      element.insertAdjacentHTML(
-        "afterbegin",
-        '<div id="spc_block'+id+'" class="w-100 py-1"></div>'
-      );
-    })
+    .then(
+      $.ajax({
+        url: 'http://localhost:3000/todo/',
+        type: 'GET',
+        dataType: 'json',
+        success: function (response) {
+          console.log(response)
+        }
+      })
+      .then(function (response){
+        var length = response.length
+        var id = response[length-1].id
+        var e = response[length-1].description
+        var element = document.getElementById("TODOs");
+    
+        element.insertAdjacentHTML(
+          'afterbegin',
+          '<button id="btn_block'+id+'" onclick="ajaxDelete('+ id +')" class="btn btn-outline-primary col-md-2">Delete</button>'
+        );
+        element.insertAdjacentHTML(
+          "afterbegin", 
+          '<div id="des_block'+id+'" class="col-4 col-md-8">' + e + '</div>'
+        );
+        element.insertAdjacentHTML(
+          "afterbegin",
+          '<div id="spc_block'+id+'" class="w-100 py-1"></div>'
+        );
+      })
+    )
   });
 })
 
