c1937567b82226f780982b029646d00e25a076fa
rojomaldit <lucaserfurt@hotmail.com>
2020-01-20 17:19:33 -0300

    Separete POST and GET, and Fix the previus TODO

--- FrontEnd/js/send-data.js
+++ FrontEnd/js/send-data.js
@@ -6,48 +6,37 @@ $(document).ready(function () {
       data: {"description": $("#Add_TODO").val()},
       dataType: 'json'
     })
-    $.ajax({
-      url: 'http://localhost:3000/todo/',
-      type: 'GET',
-      dataType: 'json',
-      success: function (response) {
-        console.log(response)
-      }
-    })
-    .then(function (response){
-      var length = response.length;
-      var id = 0;
-      var e = "";
-
-      try{
-        id = response[length].id;
-        e = response[length].description;
-      }
-      catch {
-        id = response[length-1].id;
-        e = response[length-1].description;
-      } 
-
-      var element = document.getElementById("TODOs");
-  
-      element.insertAdjacentHTML(
-        'afterbegin',
-        '<button id="btn_block'+id+'" onclick="ajaxDelete('+ id +')" class="btn btn-outline-primary col-md-2">Delete</button>'
-      );
-      element.insertAdjacentHTML(
-        "afterbegin", 
-        '<div id="des_block'+id+'" class="col-4 col-md-8">' + e + '</div>'
-      );
-      element.insertAdjacentHTML(
-        "afterbegin",
-        '<div id="spc_block'+id+'" class="w-100 py-1"></div>'
-      );
-    })
-    
   });
 })
 
-function ajaxDelete(id){
+function addTodo(){
+  var element = document.getElementById("TODOs");
+  var description = $("#Add_TODO").val();
+
+  $.ajax({
+    url: 'http://localhost:3000/todo/',
+    type: 'GET',
+    dataType: 'json'
+  })
+  .then(function (response){
+    var len = response.length + 1;
+    console.log(len)
+    element.insertAdjacentHTML(
+      'afterbegin',
+      '<button id="btn_block' + len + '" onclick="deleteTodo(' + len + ')" class="btn btn-outline-primary col-md-2">Delete</button>'
+    );
+    element.insertAdjacentHTML(
+      "afterbegin", 
+      '<div id="des_block' + len + '" class="col-4 col-md-8">'  + description +  '</div>'
+    );
+    element.insertAdjacentHTML(
+      "afterbegin",
+      '<div id="spc_block' + len + '" class="w-100 py-1"></div>'
+    );
+  })
+}
+
+function deleteTodo(id){
   $.ajax({
     url: 'http://localhost:3000/todo/' + id + '/',
     type: 'DELETE',
@@ -63,4 +52,34 @@ function ajaxDelete(id){
   btn.remove()
   des.remove()
   spc.remove()
+}
+
+function addTodos(){
+  $.ajax({
+    url: 'http://localhost:3000/todo/',
+    type: 'GET',
+    dataType: 'json',
+    success: function (response) {
+      console.log(response)
+    }
+  })
+  .then(function (response){
+    
+    var element = document.getElementById("TODOs");
+
+    response.forEach(todo => {
+      element.insertAdjacentHTML(
+        'afterbegin',
+        '<button id="btn_block' + todo.id + '" onclick="deleteTodo(' + todo.id + ')" class="btn btn-outline-primary col-md-2">Delete</button>'
+      );
+      element.insertAdjacentHTML(
+        "afterbegin", 
+        '<div id="des_block'+ todo.id + '" class="col-4 col-md-8">' + todo.description + '</div>'
+      );
+      element.insertAdjacentHTML(
+        "afterbegin",
+        '<div id="spc_block' + todo.id + '" class="w-100 py-1"></div>'
+      );
+    });
+  })
 }
