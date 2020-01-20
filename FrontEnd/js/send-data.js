bcdd3aedd70e12dd85833ce60738575954d9173a
rojomaldit <lucaserfurt@hotmail.com>
2020-01-20 17:39:21 -0300

    Fix code style

--- FrontEnd/js/send-data.js
+++ FrontEnd/js/send-data.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-  $("#ajaxSubmit").click(function post(){
+  $("#ajaxSubmit").click(function (){
     $.ajax({
       url: 'http://localhost:3000/todo/',
       type: 'POST',
       data: {"description": $("#Add_TODO").val()},
       dataType: 'json'
     })
+    .then(addTodo())
   });
 })
 
@@ -19,20 +20,7 @@ function addTodo(){
     dataType: 'json'
   })
   .then(function (response){
-    var len = response.length + 1;
-    
-    element.insertAdjacentHTML(
-      'afterbegin',
-      '<button id="btn_block' + len + '" onclick="deleteTodo(' + len + ')" class="btn btn-outline-primary col-md-2">Delete</button>'
-    );
-    element.insertAdjacentHTML(
-      "afterbegin", 
-      '<div id="des_block' + len + '" class="col-4 col-md-8">'  + description +  '</div>'
-    );
-    element.insertAdjacentHTML(
-      "afterbegin",
-      '<div id="spc_block' + len + '" class="w-100 py-1"></div>'
-    );
+    insertHTML(response.length + 1, description);
   })
 }
 
@@ -58,28 +46,27 @@ function addTodos(){
   $.ajax({
     url: 'http://localhost:3000/todo/',
     type: 'GET',
-    dataType: 'json',
-    success: function (response) {
-      console.log(response)
-    }
+    dataType: 'json'
   })
   .then(function (response){
-
-    var element = document.getElementById("TODOs");
-
     response.forEach(todo => {
-      element.insertAdjacentHTML(
-        'afterbegin',
-        '<button id="btn_block' + todo.id + '" onclick="deleteTodo(' + todo.id + ')" class="btn btn-outline-primary col-md-2">Delete</button>'
-      );
-      element.insertAdjacentHTML(
-        "afterbegin", 
-        '<div id="des_block'+ todo.id + '" class="col-4 col-md-8">' + todo.description + '</div>'
-      );
-      element.insertAdjacentHTML(
-        "afterbegin",
-        '<div id="spc_block' + todo.id + '" class="w-100 py-1"></div>'
-      );
+      insertHTML(todo.id, todo.description)
     });
   })
+}
+
+function insertHTML(id, description){
+  var element = document.getElementById("TODOs");
+  element.insertAdjacentHTML(
+    'afterbegin',
+    '<button id="btn_block' + id + '" onclick="deleteTodo(' + id + ')" class="btn btn-outline-primary col-md-2">Delete</button>'
+  );
+  element.insertAdjacentHTML(
+    "afterbegin", 
+    '<div id="des_block'+ id + '" class="col-4 col-md-8">' + description + '</div>'
+  );
+  element.insertAdjacentHTML(
+    "afterbegin",
+    '<div id="spc_block' + id + '" class="w-100 py-1"></div>'
+  );
 }
